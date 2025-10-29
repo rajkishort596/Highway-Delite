d83cf86a1bb5623635048600dcb2b5c5d95240e9
Rajkishor Thakur <rajkishort596@gmail.com>
2025-10-29 11:55:00 +0530

    Implemented DB connection and basic express server setup.

---
+++ server/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Highway Delite Api is running");
+});
+
+export { app };
